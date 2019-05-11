24e2092e0071846eb15b527d9e1df576bda1f694
Alex <awalte11@students.towson.edu>
2019-05-11 14:23:00 -0400

    Angular on git pages confirmed working. Uploading dummy files for various services and components.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListItemComponent } from './task-list-item/task-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTaskComponent,
+    TaskDetailsComponent,
+    TaskListComponent,
+    TaskListItemComponent
   ],
   imports: [
     BrowserModule,
